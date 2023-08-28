980505fa2d9c51749310ad627fe2f3ccd88515d9
Edu <107009101+educoppi@users.noreply.github.com>
2023-08-28 17:09:10 -0300

    classeDeConexao

---
+++ pi-serasa-LinkeDev/Conexao.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pi_serasa_LinkeDev
+{
+    internal class Conexao
+    {
+        const string host = "localhost";
+        const string banco = "linkedev";
+        const string usuario = "root";
+        const string senha = "senac";
+
+        const string dadosConexao = $"Server={host};Database={banco};Uid={usuario};Pwd={senha};";
+        static MySqlConnection conexao = new MySqlConnection(dadosConexao);
+
+        public static DataTable executaQuery(string query)
+        {
+            // aaaaaaaaaaaaa
+            try
+            {
+                conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand(query, conexao);
+                MySqlDataReader dados = comando.ExecuteReader();
+
+                DataTable tabela = new DataTable();
+
+                tabela.Load(dados);
+                return tabela;
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine("Erro ao realizar consulta: ");
+                Console.WriteLine(erro.Message);
+                return null;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+    }
+}
--- pi-serasa-LinkeDev/Form1.Designer.cs
+++ pi-serasa-LinkeDev/Form1.Designer.cs
@@ -28,32 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(150, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(312, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button button1;
+        private Label label1;
     }
 }
